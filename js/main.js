817197a081eaa87e6d4aafe615aba628809a9730
Bos, Joost <joost.bos@hotmail.com>
2022-06-30 11:59:27 +0200

    Working forehead detection (sometimes strange behaviour). All async functions. Gaming part not implemented yet

--- js/main.js
+++ js/main.js
@@ -1,169 +1,22 @@
-const videoWidth = 600;
-const videoHeight = 500;
-const frameRate = 100;
-const imageScaleFactor = 0.8;
-const flipHorizontal = false;
+// const videoWidth = 600;
+// const videoHeight = 500;
+const videoWidth = window.screen.width;
+const videoHeight = window.screen.height;
+const imageScaleFactor = 0.5;
+const flipHorizontal = true; // since images are being fed from a webcam
 const outputStride = 16;
+const minPartConfidence = 0.5;
+const minPoseConfidence = 0.1;
 
-window.onload = function(){
-
-    var canvas = document.querySelector('canvas');
-    canvas.width  = videoWidth
-    canvas.height = videoHeight
-
-    var context = canvas.getContext('2d');
-
-    // Add starting image and text to canvas
-    var imageObj = new Image();
-    imageObj.onload = function(){
-        context.drawImage(imageObj, 0, 0, videoWidth, videoHeight);
-        context.font = "40pt Calibri";
-        context.fillStyle = "#00ff00";
-        context.fillText("Start Game!", 150, 120);
-    };
-    imageObj.src = "./media/deloitte.jpeg";
-
-    // Remove loading block and show game
-    document.getElementById('loading').style.display = 'none';
-    document.getElementById('main').style.display = 'block';
-
-    canvas.addEventListener('click', function() {
-        startGame(canvas, context)
-    }, false);
-
-};
-
-// function loadModel() {
-//     const net = await posenet.load();
-
-//     // const net = await posenet.load({
-//     //     inputResolution: { width: 640, height: 480 },
-//     //     scale: 0.8,
-//     //   });
-//     console.log('model is loaded')
-//     return net
-// }
-
-function isAndroid() {
-    return /Android/i.test(navigator.userAgent);
-  }
-  
-  function isiOS() {
-    return /iPhone|iPad|iPod/i.test(navigator.userAgent);
-  }
-  
-  function isMobile() {
-    return isAndroid() || isiOS();
-  }
-  
-  /**
-   * Loads a the camera to be used in the demo
-   *
-   */
-async function setupCamera() {
-    if (!navigator.mediaDevices || !navigator.mediaDevices.getUserMedia) {
-      throw 'Browser API navigator.mediaDevices.getUserMedia not available';
-    }
-  
-    const video = document.getElementById('video');
-    video.width = videoWidth;
-    video.height = videoHeight;
-  
-    const mobile = isMobile();
-    const stream = await navigator.mediaDevices.getUserMedia({
-      'audio': false,
-      'video': {
-        facingMode: 'user',
-        width: mobile ? undefined : videoWidth,
-        height: mobile ? undefined: videoHeight}
-    });
-    video.srcObject = stream;
-  
-    return new Promise(resolve => {
-      video.onloadedmetadata = () => {
-        resolve(video);
-      };
-    });
-}
-  
-async function loadVideo() {
-    const video = await setupCamera();
-    video.play();
-  
-    return video;
+window.onload = function() {
+    startScreen()
 }
 
-function draw(video, canvas, context, frameRate) {
-    // Draw video on canvas
-    context.drawImage(video, 0, 0, canvas.width, canvas.height);
-    // Make sure the contxt gets updated with the latest video image
-    setTimeout(draw, 1/frameRate, video, canvas, context, frameRate);
-}
-
-/**
- * Draw pose keypoints onto a canvas
- */
-function drawKeypoints(keypoints, minConfidence, context, scale = 1) {
-    for (let i = 0; i < keypoints.length; i++) {
-      const keypoint = keypoints[i];
-  
-      if (keypoint.score < minConfidence) {
-        continue;
-      }
-  
-      const { y, x } = keypoint.position;
-      context.beginPath();
-      context.arc(x * scale, y * scale, 3, 0, 2 * Math.PI);
-      context.fillStyle = "#00ff00";
-      context.fill();
-    }
-}
-
-/**
- * Draws a line on a canvas, i.e. a joint
- */
-function drawSegment([ay, ax], [by, bx], scale, ctx) {
-    ctx.beginPath();
-    ctx.moveTo(ax * scale, ay * scale);
-    ctx.lineTo(bx * scale, by * scale);
-    ctx.lineWidth = 1;
-    ctx.strokeStyle = "#00ff00";
-    ctx.stroke();
-}
-
-function toTuple({ y, x }) {
-    return [y, x];
-}
-
-/**
- * Draws a pose skeleton by looking up all adjacent keypoints/joints
- */
-function drawSkeleton(keypoints, minConfidence, ctx, scale = 1) {
-    const adjacentKeyPoints = posenet.getAdjacentKeyPoints(
-      keypoints,
-      minConfidence
-    );
-  
-    adjacentKeyPoints.forEach((keypoints) => {
-      drawSegment(
-        toTuple(keypoints[0].position),
-        toTuple(keypoints[1].position),
-        scale,
-        ctx
-      );
-    });
-}
-
-
-async function detectPoseInRealTime(canvas, context, video, net) {
+async function detectPoseInRealTime(canvas, context, video, net, image) {
 
     async function poseDetectionFrame() {
-        console.log('detecting pose')
     
         pose = await net.estimateSinglePose(video, imageScaleFactor, flipHorizontal, outputStride);
-        console.log(pose)
-        console.log(pose.keypoints)
-        console.log(pose.minPartConfidence)
     
         context.clearRect(0, 0, videoWidth, videoHeight);
     
@@ -172,20 +25,24 @@ async function detectPoseInRealTime(canvas, context, video, net) {
         context.translate(-videoWidth, 0);
         context.drawImage(video, 0, 0, videoWidth, videoHeight);
         context.restore();
+
+        context.drawImage(image, 400, 200, 100, 100);
+        console.log(image)
     
         // Draw keypoints on canvas
-        drawKeypoints(pose.keypoints, pose.minPartConfidence, context);
+        drawKeypoints(pose.keypoints, minPartConfidence, context);
+
+        // Draw forehead keypoint on canvas
+        drawForeheadKeypoint(pose.keypoints, minPartConfidence, context)
     
         // draw skeleton on canvas
-        drawSkeleton(pose.keypoints, pose.minPartConfidence, context);
+        //drawSkeleton(pose.keypoints, minPoseConfidence, context);
     
         requestAnimationFrame(poseDetectionFrame);
     }
 
     poseDetectionFrame();
 
-    // // Make sure the function runs for the latest video image
-    // setTimeout(detectPoseInRealTime, 1/frameRate, video, canvas, context, frameRate);
 }
 
 async function startGame(canvas, context) {
@@ -210,6 +67,34 @@ async function startGame(canvas, context) {
     // Clear the canvas
     context.clearRect(0, 0, videoWidth, videoHeight);
 
-    detectPoseInRealTime(canvas, context, video, net)
+    // Load image of the game
+    const image = await loadImage('./media/deloitte_logo.jpeg')
 
+    detectPoseInRealTime(canvas, context, video, net, image)
+
+}
+
+async function startScreen(){
+
+    var canvas = document.querySelector('canvas');
+    canvas.width  = videoWidth
+    canvas.height = videoHeight
+
+    var context = canvas.getContext('2d');
+
+    // Add starting image and text to canvas
+    const image = await loadImage('./media/deloitte.jpeg')
+
+    context.drawImage(image, 0, 0, videoWidth, videoHeight);
+    context.font = "40pt Calibri";
+    context.fillStyle = "#00ff00";
+    context.fillText("Start Game!", 150, 120);
+
+    // Remove loading block and show game
+    document.getElementById('loading').style.display = 'none';
+    document.getElementById('main').style.display = 'block';
+
+    canvas.addEventListener('click', function() {
+        startGame(canvas, context)
+    }, false);
 }
---
+++ js/utils.js
@@ -0,0 +1,147 @@
+function isAndroid() {
+    return /Android/i.test(navigator.userAgent);
+  }
+  
+  function isiOS() {
+    return /iPhone|iPad|iPod/i.test(navigator.userAgent);
+  }
+  
+  function isMobile() {
+    return isAndroid() || isiOS();
+  }
+  
+/**
+ * Loads a the camera to be used in the demo
+ */
+async function setupCamera() {
+    if (!navigator.mediaDevices || !navigator.mediaDevices.getUserMedia) {
+      throw 'Browser API navigator.mediaDevices.getUserMedia not available';
+    }
+  
+    const video = document.getElementById('video');
+    video.width = videoWidth;
+    video.height = videoHeight;
+  
+    const mobile = isMobile();
+    const stream = await navigator.mediaDevices.getUserMedia({
+      'audio': false,
+      'video': {
+        facingMode: 'user',
+        width: mobile ? undefined : videoWidth,
+        height: mobile ? undefined: videoHeight}
+    });
+    video.srcObject = stream;
+  
+    return new Promise(resolve => {
+      video.onloadedmetadata = () => {
+        resolve(video);
+      };
+    });
+}
+  
+async function loadVideo() {
+    const video = await setupCamera();
+    video.play();
+  
+    return video;
+}
+
+/**
+ * Draw forehead keypoint onto a canvas
+ */
+function drawForeheadKeypoint(keypoints, minConfidence, context) {
+    let leftEye;
+    let rightEye;
+    let nose;
+
+    for (let i = 0; i < keypoints.length; i++) {
+
+        if (keypoints[i].part === 'leftEye') {
+            leftEye = keypoints[i];
+        } 
+        else if (keypoints[i].part === 'rightEye') {
+            rightEye = keypoints[i];
+        }
+        else if (keypoints[i].part === 'nose') {
+            nose = keypoints[i];
+        }
+    }
+    
+    if (leftEye.score > minConfidence && rightEye.score > minConfidence && nose.score > minConfidence) {
+        // x_midPoint = 0.5 * leftEye.position.x + 0.5 * rightEye.position.x
+        let x = leftEye.position.x + rightEye.position.x - nose.position.x
+        let y = leftEye.position.y + rightEye.position.y - nose.position.y
+
+        x = videoWidth - x; // Bug fix with horizontal flip
+        context.beginPath();
+        context.arc(x, y, 16, 0, 2 * Math.PI);
+        context.fillStyle = "rgb(220,20,60, 0.4)";
+        context.fill();
+    }
+}
+
+/**
+ * Draw pose keypoints onto a canvas
+ */
+function drawKeypoints(keypoints, minConfidence, context, scale = 1) {
+    for (let i = 0; i < keypoints.length; i++) {
+      const keypoint = keypoints[i];
+  
+      if (keypoint.score < minConfidence) {
+        continue;
+      }
+  
+      let { y, x } = keypoint.position;
+      x = videoWidth - x; // Bug fix with horizontal flip
+      context.beginPath();
+      context.arc(x * scale, y * scale, 3, 0, 2 * Math.PI);
+      context.fillStyle = "#00ff00";
+      context.fill();
+    }
+}
+
+/**
+ * Draws a line on a canvas, i.e. a joint
+ */
+function drawSegment([ay, ax], [by, bx], scale, context) {
+    context.beginPath();
+    context.moveTo(ax * scale, ay * scale);
+    context.lineTo(bx * scale, by * scale);
+    context.lineWidth = 1;
+    context.strokeStyle = "#00ff00";
+    context.stroke();
+}
+
+function toTuple({ y, x }) {
+    return [y, x];
+}
+
+/**
+ * Draws a pose skeleton by looking up all adjacent keypoints/joints
+ */
+function drawSkeleton(keypoints, minConfidence, context, scale = 1) {
+    const adjacentKeyPoints = posenet.getAdjacentKeyPoints(
+      keypoints,
+      minConfidence
+    );
+  
+    adjacentKeyPoints.forEach((keypoints) => {
+      drawSegment(
+        toTuple(keypoints[0].position),
+        toTuple(keypoints[1].position),
+        scale,
+        context
+      );
+    });
+}
+
+async function loadImage(url) {
+    return new Promise((resolve, reject) => {
+        const img = new Image();
+        img.addEventListener('load', () => resolve(img));
+        img.addEventListener('error', () => {
+            reject(new Error(`Failed to load ${url}`));
+        });
+        img.src = url;
+    });
+}
